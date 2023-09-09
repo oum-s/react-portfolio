656f40613a47b34c1f01313f0de343945fd19151
oumi youss <oumiyouss@hotmail.fr>
2023-09-09 23:26:40 +0200

    about modifié

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -29,7 +29,7 @@ function About({showNav}){
 
                       En parallèle, j'ai créé un site à l'aide de Wordpress sur le développement personnel. J'ai passé des journées à écrire des articles, régler les petits problèmes techniques du site, automatiser les mails de ma newsletter, faire du référencement naturel et communiquer avec ma petite communauté. J'en apprenais tous les jours et l'envie de travailler dans le domaine du numérique ne cessait d'augmenter.
                       <br /> <br />
-                      Puis en 2021, j'ai enfin sauté le pas et fait une formation dans le développement web ! J'ai appris en quelques mois des langages tels que Javascript, PHP, Ionic, Angular... Les journées ont été intenses, mon cerveau n'en pouvait plus, mais ma passion ne cessait de grandir. J'ai pu approfondir mes connaissances lors de deux stages dans lesquels j'ai développé mes compétences en Wordpress et Php. Puis j'ai fait une deuxième formation pendant laquelle j'ai pu découvrir React, ce qui m'a permis d'obtenir mon titre RNCP de développeur web.
+                      Puis en 2021, j'ai enfin sauté le pas et fait une formation dans le développement web ! Les journées ont été intenses, mon cerveau n'en pouvait plus, mais ma passion ne cessait de grandir. Puis j'ai fait une deuxième formation pendant laquelle j'ai pu découvrir React, ce qui m'a permis d'obtenir mon titre RNCP de développeur web.
                       <br /> <br />
                       Ce que je trouve le plus intéressant dans mon parcours reste le fait qu'on est et restera des étudiants toute notre vie si on veut continuer à évoluer, surtout dans le numérique. 
                     
