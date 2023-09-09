f7406033599545ce29b9492334bf331016bf561c
oumi youss <oumiyouss@hotmail.fr>
2023-09-09 23:33:16 +0200

    modif présentation

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -25,13 +25,11 @@ function About({showNav}){
                     <p>
                       Hello, c'est Oumi ! Et si je partageais un peu mon histoire ? 
                       <br /> <br />
-                      Une fois ma licence en poche, j'ai travaillé en tant qu'animatrice dans une association. Mon rôle principal était d'enseigner la langue arabe à des enfants. Cette expérience m'a appris la pédagogie, la patience (on n'a pas le choix face à des enfants) et m'a rappelé l'importance de l'apprentissage ! <br /> <br />
-
-                      En parallèle, j'ai créé un site à l'aide de Wordpress sur le développement personnel. J'ai passé des journées à écrire des articles, régler les petits problèmes techniques du site, automatiser les mails de ma newsletter, faire du référencement naturel et communiquer avec ma petite communauté. J'en apprenais tous les jours et l'envie de travailler dans le domaine du numérique ne cessait d'augmenter.
+                      Une fois ma licence en poche, j'ai été enseignante dans une association. Cette expérience m'a rappelé l'importance de l'apprentissage ! En parallèle, j'ai créé un site à l'aide de Wordpress sur le développement personnel. J'ai passé des journées à écrire des articles, régler les petits problèmes techniques du site, automatiser les mails de ma newsletter, faire du référencement naturel et communiquer avec ma petite communauté. J'en apprenais tous les jours et l'envie de travailler dans le domaine du numérique ne cessait d'augmenter.
                       <br /> <br />
                       Puis en 2021, j'ai enfin sauté le pas et fait une formation dans le développement web ! Les journées ont été intenses, mon cerveau n'en pouvait plus, mais ma passion ne cessait de grandir. Puis j'ai fait une deuxième formation pendant laquelle j'ai pu découvrir React, ce qui m'a permis d'obtenir mon titre RNCP de développeur web.
-                      <br /> <br />
-                      Ce que je trouve le plus intéressant dans mon parcours reste le fait qu'on est et restera des étudiants toute notre vie si on veut continuer à évoluer, surtout dans le numérique. 
+                      <br />
+                      Aujourd'hui j'espère travailler dans une entreprise qui me correspond et qui m'aidera à évoluer. 
                     
                     </p>
 
